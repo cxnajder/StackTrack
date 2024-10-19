2bcaec51ae86f95a18adef00f1172b566a7b4ddd
cxnajder <259448@student.pwr.edu.pl>
2024-10-19 09:33:44 +0200

    Add artificial delay to test

--- StackTrack.test.cpp
+++ StackTrack.test.cpp
@@ -1,8 +1,16 @@
 #include "StackTrack.macro.h"
+#include <thread>
+#include <chrono>
+
+void artificialDelay()
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+}
 
 void buzz()
 {
     STACK_TRACK_THIS_FUNCTION
+    artificialDelay();
 }
 
 void bar()
